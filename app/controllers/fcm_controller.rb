3d3280c8e3a96b80a44c613f2c26ce995a5ab9f2
Hari Prasad <dnshariprasad@gmail.com>
2016-08-31 23:06:25 +0530

    deys updated

--- app/controllers/fcm_controller.rb
+++ app/controllers/fcm_controller.rb
@@ -1,8 +1,8 @@
 require 'fcm'
 class FcmController < ApplicationController
   def sendPushNote
-    fcm = FCM.new("1:776651629509:android:3bb1e8b0a3e6ec29")
-    registration_ids= ["fuOeAA-fPwI:APA91bFiUr7nNKOkXZEQYswuXq7AuQ4Hzjhw_OO-V3TGC3BnHNueZnvt8noE-35-MtQ_3qKgGig7pvYQZCBjVVSaWZPqrS8WRoNShZ0AhWPW-ByurLtBaNwVg6Zd_cIiv_0IFgSpn77g"] # an array of one or more client registration tokens
+    fcm = FCM.new("1:671712274104:android:3bb1e8b0a3e6ec29")
+    registration_ids= ["ckWPTGfnI4E:APA91bGRdoUxYVSHqgzxub0guMPdpUkpJyo9FbNfGIct_rXBBm3D7CDCtQomf9Bbneolbx2q8yY2p8UCrsaCCnaZdEvD0BbNhPXJExTXBfbn2TZvuPZtvfenaHwqWmFArxygI_A4ApKP"] # an array of one or more client registration tokens
     options = {data: {score: "123"}, collapse_key: "updated_score"}
     response = fcm.send(registration_ids, options)
     case response[:status]
@@ -11,6 +11,5 @@ class FcmController < ApplicationController
       else
         render json: {response: response[:body], status_code: response[:status], headers: response[:headers]}, statu: :un_processible_entity
       end # when
-    # render json: {response: "success"}, status: :ok
   end # sendPush
 end # class
